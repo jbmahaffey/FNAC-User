cc388d28d516686e78dd6ee80b74d0e82bcd5779
jbmahaffey <84487961+jbmahaffey@users.noreply.github.com>
2023-07-19 15:58:43 -0700

    Update nac-add-user.py

--- nac-add-user.py
+++ nac-add-user.py
@@ -24,9 +24,23 @@ def setuser(nacserver, apikey, newuser, newpass):
     req = requests.post(url=url, data=json.dumps(data), headers=headers, verify=False)
     print(req.status_code)
 
+def email(nacserver, apikey):
+    headers = {
+        'Authorization': 'Bearer {}'.format(apikey),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    url = 'https://{}:8443/api/v2/settings/integration/email'.format(nacserver)
+
+    data = {'emailServer': 'nsrelay.ccsd.net', 'emailSender': 'NAC-DoNotReply@nv.ccsd.net'}
+
+    req = requests.post(url=url, data=data, headers=headers, verify=False)
+    print(req.status_code)
+
 if __name__ == '__main__':
     nacserver = input('Enter NAC Server IP: ')
     apikey = input('Enter NAC API Key: ')
     newuser = input('Enter New User Name: ')
     newpass = pwinput.pwinput(prompt='Please enter your New User password: ')
     setuser(nacserver, apikey, newuser, newpass)
+    email(nacserver, apikey)
